d2ffdb3dfdf31dadff765e0b4b2c27c290346dda
Florencia Lopez <florenciamariellopez@hotmail.com.ar>
2023-03-01 23:51:15 -0300

    proyecto

---
+++ script.js
@@ -0,0 +1,5 @@
+function masInfo(){
+    alert("En caso de requerir mayor información por favor enviar un correo a biancadacosta33@gmail.com");
+}
+
+document.getElementById("Mas_info").addEventListener("click", masInfo);
